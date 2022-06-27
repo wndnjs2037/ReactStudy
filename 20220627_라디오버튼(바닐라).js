e98c3645127e319070acac3ea46e7778e892534c
Juwon <71490091+wndnjs2037@users.noreply.github.com>
2022-06-28 00:04:14 +0900

    Create 20220627_라디오버튼(바닐라).js

---
+++ 20220627_라디오버튼(바닐라).js
@@ -0,0 +1,10 @@
+<div className="review_select_box">
+
+  <p>후기 구분</p>
+  {/* checked={this.state.selectValue === "리얼 후기"}  onChange={this.handleChange2} */}
+  <input className="radio_real" type='radio' value='리얼 후기' />
+  <label for="리얼 후기">리얼 후기</label>
+  <input className="radio_success" type='radio' value='성공 후기'/>
+  <label for="성공 후기">성공 후기</label>
+
+</div>
